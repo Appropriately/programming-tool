c2b51303ba774939457a87814b05a7d9d71be691
Sean Lewis <sean.alan.lewis@gmail.com>
2020-02-18 15:58:37 +0000

    Add a starfield to the game

---
+++ Assets/Scripts/StarfieldGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarfieldGenerator : MonoBehaviour
+{
+    private const int STARS = 2000;
+    private const float DISTANCE = 35.0f;
+
+    private ParticleSystem system;
+    private ParticleSystem.Particle[] particles;
+
+    private void Start()
+    {
+        particles = new ParticleSystem.Particle[STARS];
+
+        for (int i = 0; i < STARS; i++)
+        {
+            particles[i].position = Random.insideUnitSphere * DISTANCE;
+            while (Vector3.Distance(Vector3.zero, particles[i].position) < DISTANCE * 0.9f)
+            {
+                particles[i].position = Random.insideUnitSphere * DISTANCE;
+            }
+            particles[i].startSize = Random.Range(0.01f, 0.05f);
+            particles[i].startColor = new Color(1, 1, 1, 1);
+        }
+
+        system = gameObject.GetComponent<ParticleSystem>();
+        system.SetParticles(particles, particles.Length);
+    }
+}
